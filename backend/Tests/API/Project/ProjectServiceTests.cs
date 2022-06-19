0bb45c0f3dc743b38a5a7382188d922c009b6251
Sgenne <simon.genne@gmail.com>
2022-06-19 22:47:44 +0200

    Tests

---
+++ backend/Tests/API/Project/ProjectServiceTests.cs
@@ -0,0 +1,14 @@
+using API.Project;
+using Moq;
+
+namespace Tests.API.Project;
+
+public class ProjectServiceTests
+{
+    [Fact]
+    public void CreateProject()
+    {
+        var mockRepository = new Mock<ProjectRepository>();
+      // Setup
+    }
+}
