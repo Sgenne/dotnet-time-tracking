1d40e366617126bd08bcd500d78d0639b4803079
Sgenne <simon.genne@gmail.com>
2022-07-15 13:25:57 +0200

    Worked on reducer in LoginGuard.tsx

--- timetracking-frontend/auth/LoginGuard.tsx
+++ timetracking-frontend/auth/LoginGuard.tsx
@@ -1,23 +1,47 @@
-import { useRouter } from "next/router";
-import { ReactElement, useEffect, useState } from "react";
-import { useAuthContext } from "./AuthContext";
+import { NextRouter, useRouter } from "next/router";
+import { ReactElement, useEffect, useReducer, useState } from "react";
+import { AuthContextState, useAuthContext } from "./AuthContext";
 
 const LoginGuard = ({ children }: { children: ReactElement }) => {
-  const authContext = useAuthContext();
-  const router = useRouter();
+  const authContext: AuthContextState = useAuthContext();
+  const [{ hasRedirected, redirectedFrom }, dispatch] = useReducer(
+    redirectReducer,
+    defaultReducerState
+  );
+  const router: NextRouter = useRouter();
 
-  const url = router.pathname;
+  const url: string = router.pathname;
 
   useEffect(() => {
-    if (!authContext.isSignedIn && url !== "/login") {
+    const shouldRedirect: boolean = !authContext.isSignedIn && url !== "/login";
+
+    if (shouldRedirect) {
       router.push("/login");
+      dispatch(redirectToLoginAction(url));
     }
   }, [authContext.isSignedIn, router, url]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (!authContext.isSignedIn) return;
+
+    const newUrl: string = hasRedirected ? redirectedFrom : "/";
+
+    router.push(newUrl);
+    dispatch(redirectFromLoginAction());
+  }, [authContext.isSignedIn, hasRedirected, redirectedFrom, router]);
 
   return children;
 };
+
+const redirectToLoginAction = (redirectFromUrl: string): RedirectAction => ({
+  type: RedirectActionType.REDIRECT_TO_LOGIN,
+  payload: { redirectedFrom: redirectFromUrl },
+});
+
+const redirectFromLoginAction = (): RedirectAction => ({
+  type: RedirectActionType.REDIRECT_FROM_LOGIN,
+  payload: {},
+});
 
 interface RedirectState {
   hasRedirected: boolean;
@@ -29,8 +53,13 @@ const defaultReducerState: RedirectState = {
   redirectedFrom: "",
 };
 
+enum RedirectActionType {
+  REDIRECT_TO_LOGIN = "REDIRECT_TO_LOGIN",
+  REDIRECT_FROM_LOGIN = "REDIRECT_FROM_LOGIN",
+}
+
 interface RedirectAction {
-  type: string;
+  type: RedirectActionType;
   payload: { redirectedFrom?: string };
 }
 
@@ -41,7 +70,7 @@ const redirectReducer = (
   const type = action.type;
   const payload = action.payload;
 
-  if (type === "REDIRECT_TO_LOGIN" && payload.redirectedFrom) {
+  if (type === RedirectActionType.REDIRECT_TO_LOGIN && payload.redirectedFrom) {
     return {
       ...state,
       hasRedirected: true,
@@ -49,7 +78,7 @@ const redirectReducer = (
     };
   }
 
-  if (type === "REDIRECT_FROM_LOGIN") {
+  if (type === RedirectActionType.REDIRECT_FROM_LOGIN) {
     return defaultReducerState;
   }
 
