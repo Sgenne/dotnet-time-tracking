0a553a13a22a0b09ad45aa1069e2450c76a9e33a
Sgenne <simon.genne@gmail.com>
2022-07-21 13:29:18 +0200

    Created default layout with vertical navbar.

--- timetracking-frontend/components/layouts/DefaultLayout.tsx
+++ timetracking-frontend/components/layouts/DefaultLayout.tsx
@@ -1,12 +1,20 @@
 import React, { ReactElement } from "react";
 import styles from "../../styles/layouts/DefaultLayout.module.css";
+import VerticalNavbar from "./navbar/VerticalNavbar";
 
 export interface DefaultLayoutProps {
   children: ReactElement;
 }
 
 const DefaultLayout = ({ children }: DefaultLayoutProps) => {
-  return <div className={styles["page-container"]}>{children}</div>;
+  return <div className={styles["page-container"]}>
+    <div className={styles["navbar-container"]}>
+      <VerticalNavbar />
+    </div>
+    <div className={styles["content-container"]}>
+      {children}
+    </div>
+  </div>;
 };
 
 export default DefaultLayout;
---
+++ timetracking-frontend/components/layouts/navbar/VerticalNavbar.tsx
@@ -0,0 +1,11 @@
+import styles from "../../../styles/layouts/navbar/VerticalNavbar.module.css"
+
+const VerticalNavbar = () => {
+    return (
+        <nav className={styles["navbar"]}>
+
+        </nav>
+    )
+}
+
+export default VerticalNavbar
