58fc34bb4aeb1e4212ea3c67227b818398353d71
Sgenne <simon.genne@gmail.com>
2022-08-04 17:58:22 +0200

    Moved logic from TextInputStateHandler to useStringInput.

--- timetracking-frontend/hooks/useStringInput.ts
+++ timetracking-frontend/hooks/useStringInput.ts
@@ -1,16 +1,52 @@
 import { useState } from "react";
 import ControlledStateHandler from "../types/ControlledStateHandler";
 import OnStringChange from "../types/OnStringChange";
+import Validator from "../types/Validator";
+import { noOpValidator } from "../utils/validators/NoOpValidator";
 
-const useStringInput = (initialValue = ""): ControlledStateHandler<string> => {
-  const [value, setValue] = useState(initialValue);
+export interface useStringInputProps {
+  defaultValue?: string;
+  validator?: Validator<string>;
+}
 
-  const changeHandler: OnStringChange = (newValue: string) =>
+const useStringInput = ({
+  defaultValue = "",
+  validator = noOpValidator,
+}: useStringInputProps): ControlledStateHandler<string> => {
+  const [value, setValue] = useState(defaultValue);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [isTouched, setIsTouched] = useState(false);
+
+  const changeHandler: OnStringChange = (newValue: string) => {
+    setIsTouched(true);
     setValue(newValue);
+    if (hasError) validate();
+  };
+
+  const blurHandler = () => {
+    if (!isTouched) return;
+
+    validate();
+  };
+
+  const validate = () => {
+    const { message, isValid } = validator(value);
+    if (!isValid) {
+      setHasError(true);
+      setErrorMessage(message);
+      return;
+    }
+    setHasError(false);
+    setErrorMessage(undefined);
+  };
 
   return {
     value,
     onChange: changeHandler,
+    onBlur: blurHandler,
+    hasError,
+    errorMessage,
   };
 };
 
